4d71bcb68c12cc6adea539b2d5e8172bb654b8c9
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2019-06-27 16:12:04 -0300

    WIP: exception and logging options

--- src/is_face_detector/utils.py
+++ src/is_face_detector/utils.py
@@ -6,7 +6,13 @@ import sys
 def load_options():
     log = Logger(name='LoadingOptions')
     op_file = sys.argv[1] if len(sys.argv) > 1 else 'options.json'
-    with open (op_file, 'r') as f:
-        op = Parse(f.read(), FaceDetectorOptions())   
-    return op
-
+    try:
+        with open (op_file, 'r') as f:
+            try:
+                op = Parse(f.read(), FaceDetectorOptions())
+                log.info('FaceDetectorOptions: \n{}', op)   
+                return op
+            except Exception as ex:
+                log.critical('Unable to load options from \'{}\'. \n{}', op_file, ex)
+    except Exception as ex:
+        log.critical('Unable to open file \'{}\'', op_file)
