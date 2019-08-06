3860d64da5c196f8ed1c9d6538be755a82e039e2
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2019-08-06 19:10:02 -0300

    WIP: edit __init__.py file

--- src/is_face_detector/__init__.py
+++ src/is_face_detector/__init__.py
@@ -1,8 +1,10 @@
 from is_face_detector.face_detector import FaceDetector
+from is_face_detector.service_channel import ServiceChannel
 from is_face_detector.options_pb2 import FaceDetectorOptions
 from is_face_detector.utils import load_options
 from is_face_detector.image_tools import to_image, to_np, draw_detection
 
 __all__ = [
-    "FaceDetector", "FaceDetectorOptions", "load_options", "to_image", "to_np", "draw_detection"
+    "FaceDetector", "FaceDetectorOptions", "load_options", "to_image", "to_np", "draw_detection",
+    "ServiceChannel"
 ]
